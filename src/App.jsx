6a039f6b9ccb6b08bc1359633a62b893852759f8
Ekatmoz <ekatmoz@gmail.com>
2024-12-17 14:42:05 +0100

    added alert

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Navigation from './Components/Navigation';
+import Hero from './Components/Hero';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <h1>Hello World!</h1>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Navigation/>
+      <Hero/>
+    </div> 
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {},
+    fontFamily: {
+      Lato: ["Lato", "sans-serif"],
+      Mplus: ["M PLUS Rounded 1c", "sans-serif"]
+    }
   },
   plugins: [],
 }
